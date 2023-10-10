619801bb3610c1731854778849ae601328ce530d
Kielson Zinn da Silva <kielsonzinn>
2023-10-09 21:36:41 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, request, redirect, session, url_for, render_template
+from requests_oauthlib import OAuth2Session
+import requests
+import os 
+
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+app = Flask(__name__)
+app.secret_key = 'REPLACE_REAL_DATA'
+
+client_id = 'REPLACE_REAL_DATA'
+client_secret = 'REPLACE_REAL_DATA'
+authorization_base_url = 'https://github.com/login/oauth/authorize'
+token_url = 'https://github.com/login/oauth/access_token'
+
+def clear_session():
+    session.clear()
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/github_login')
+def github_login():
+    if 'oauth_token' in session:
+        return redirect(url_for('callback'))
+    
+    github = OAuth2Session(client_id)
+    authorization_url, state = github.authorization_url(authorization_base_url)
+
+    session['oauth_state'] = state
+
+    return redirect(authorization_url)
+
+@app.route('/callback', methods=['GET'])
+def callback():
+    if 'oauth_state' not in session:
+        return redirect(url_for('index'))
+
+    github = OAuth2Session(client_id, state=session['oauth_state'])
+    token = github.fetch_token(token_url, client_secret=client_secret,
+                               authorization_response=request.url)
+
+    print(token)
+    user_info_response = github.get('https://api.github.com/user')
+
+    if user_info_response.status_code == 200:
+        user_info = user_info_response.json()
+        session['oauth_token'] = token
+        return render_template('welcome.html', user_info=user_info)
+    else:
+        return redirect(url_for('github_login'))
+
+@app.route('/logout')
+def logout():
+    session.pop('oauth_state', None)
+    session.pop('oauth_token', None)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
